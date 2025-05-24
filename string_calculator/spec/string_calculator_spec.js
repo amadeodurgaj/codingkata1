3b8bad2eb0247b2f93d988701dd17375138a8101
Amadeo Durgaj <thundahlovesjojo@gmail.com>
2025-05-24 18:15:40 +0200

    Third test

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -10,4 +10,8 @@ describe('String Calculator', function() {
 	it('should return 1 for 1', function() {
 		expect(calculator.add('1')).toEqual(1);
 	});
+
+	it('should return 3 for 1,2', function() {
+		expect(calculator.add('1,2')).toEqual(3);
+	});
 });
